bf8dfa0423a8f802bdd19d1fe4382b86e070b174
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-07 12:23:40 -0600

    lunch break - bonus 5. creating page counter

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,37 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        PrintWriter helloWorld = response.getWriter();
+//        helloWorld.println("<h1>Hello World!</h1>");
+//        System.out.println("GAHHHHHH");
+//    }
+    private int hitCount;
+
+    public void init() {
+        hitCount = 0;
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        hitCount++;
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+
+        if(name == null) {
+            out.println("<h1>Hello World!</h1>");
+            return;
+        }
+        out.println("<h1>Hello " + name +  "!</h1>");
+
+        String hitCounter = "Total # hits: ";
+        String count = request.getParameter("count");
 
     }
 }
