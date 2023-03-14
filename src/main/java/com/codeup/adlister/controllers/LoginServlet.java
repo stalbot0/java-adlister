ac6adc182b542ed0986b6b3695f71e39807bb6a1
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-14 11:07:44 -0500

    completed sessions exercise

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -11,22 +11,29 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession().getAttribute("user") != null) {
+            System.out.format("doGet1 in LoginServlet");
             response.sendRedirect("/profile");
             return;
         }
         request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+//        if (username != null && username.equals("admin") && password != null && password.equals("password")) {
+//            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+//            return;
+//        } else {
+//            response.sendRedirect("/login");
+//        }
 
         // TODO: find a record in your database that matches the submitted password
         // TODO: make sure we find a user with that username
         // TODO: check the submitted password against what you have in your database
-        boolean validAttempt = false;
 
-        if (validAttempt) {
+
+        if (username != null && username.equals("admin") && password != null && password.equals("password")) {
             // TODO: store the logged in user object in the session, instead of just the username
             request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("username") == null) {
+        if (request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
             return;
         }
