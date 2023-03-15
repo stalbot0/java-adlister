e2c660c8e3d7e111e8f9eab519366599c363a115
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-03-15 16:24:31 -0500

    updated user input exercise

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -31,7 +31,7 @@ public class MySQLUsersDao implements Users {
             User user = extractUser(rs);
             return user;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("error finding a user", e);
         }
     }
 
